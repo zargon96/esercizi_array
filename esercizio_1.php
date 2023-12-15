6af0c1e2f53b546feec913015834bbbb6c27337f
marco <marco.crucitti96@gmail.com>
2023-12-15 11:00:52 +0100

    fatto

--- esercizio_1.php
+++ esercizio_1.php
@@ -1,9 +1,22 @@
-<?php
- $dati_studenti = [
-    'nome' => 'mario',
-    'cognome' => 'rossi',
-    'età' => 20,
- ];
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>esercizio_1</title>
+</head>
+<body>
+   <?php
+   $dati_studenti = [
+      'nome' => 'mario',
+      'cognome' => 'rossi',
+      'età' => 20,
+   ];
+   echo "Dati personali:\n";
+   echo "Nome: " . $dati_studenti['nome'] . "\n";
+   echo "Cognome: " . $dati_studenti['cognome'] . "\n";
+   echo "Età: " . $dati_studenti['età'] . " anni\n";
+   ?>
+</body>
+</html>
 
- echo $dati_studenti; 
-?>
