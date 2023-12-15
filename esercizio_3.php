358a060f0b8e6022de5c7367840df46caadf50ec
marco <marco.crucitti96@gmail.com>
2023-12-15 11:29:25 +0100

    fatto

--- esercizio_3.php
+++ esercizio_3.php
@@ -3,7 +3,7 @@
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>esercizio_2</title>
+   <title>esercizio_3</title>
 </head>
 <body>
     <?php
